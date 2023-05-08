3b75c23861caa63637628f52f140e2e57d82f396
coder-1010 <60008502+coder-1010@users.noreply.github.com>
2023-05-08 11:03:24 +0530

    Add files via upload

---
+++ FindEmail.java
@@ -0,0 +1,32 @@
+package virtualClass;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FindEmail {
+    public static void main(String[] args) {
+        List<String> Email=new ArrayList<>();
+        Scanner input=new Scanner(System.in);
+        Email.add("email1@gmail.com");
+        Email.add("email2@gmail.com");
+        Email.add("email3@gmail.com");
+        Email.add("email4@gmail.com");
+        Email.add("email5@gmail.com");
+        Email.add("email6@gmail.com");
+        while(true) {
+        	System.out.print("\nExit (y/n): ");
+        	char r=input.next().charAt(0);
+        	if(r=='y')
+        		break;
+        	System.out.println("\nEnter the Email to search");
+        	input.nextLine();
+        	String Search=input.nextLine();
+            if(Email.contains(Search))
+                System.out.println("Email found");
+            else
+                System.out.println("Email not found");
+        }
+        input.close();
+    }
+}
